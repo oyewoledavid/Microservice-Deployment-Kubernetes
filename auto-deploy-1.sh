b641576cafb3173b0333101014f0a4a3c29d699a
oyewoledavid <oyewoledavid69@gmail.com>
2024-08-15 21:11:30 +0100

    All deployment set except alertmanager

--- auto-deploy-1.sh
+++ auto-deploy-1.sh
@@ -8,16 +8,23 @@ cluster_name=$(terraform output -raw cluster_name)
 # Go to terraform directory and run terraform init and terraform apply
 terraform init
 terraform apply --auto-approve
+
 # Get the kubeconfig file and update the kubeconfig
 aws eks update-kubeconfig --region $region --name $cluster_name
 
 #create namespace and set the context
-cd ../manifests
 kubectl create namespace sock-shop
 kubectl config set-context --current --namespace=sock-shop
+##helm create sock-shop
+cd ../sock-shop
+helm install sock-shop .
 
-# Deploy the sock-shop application
-kubectl apply -f .
+# Add the helm repo for ingress-nginx and install the ingress controller
+helm repo add ingress https://kubernetes.github.io/ingress-nginx
+helm repo update
+helm install ingress ingress/ingress-nginx 
+cd ..
+kubectl apply -f ingress.yaml
 
 # Install cert-manager
 helm install \
@@ -28,17 +35,14 @@ helm install \
   --set installCRDs=true
 
 # Apply the cluster issuer file to create the cluster issuer
-cd ..
 kubectl apply -f cluster-issuer.yaml
-
-
-# Add the helm repo for ingress-nginx and install the ingress controller
-helm repo add ingress https://kubernetes.github.io/ingress-nginx
-helm repo update
-helm install ingress ingress/ingress-nginx 
-kubectl apply -f ingress.yaml
 
 # Add the helm repo for prometheus and install prometheus for prometheus, grafana and alertmanager
 helm repo add prometheus https://prometheus-community.github.io/helm-charts
 helm repo update
 helm install prometheus prometheus/kube-prometheus-stack
+
+kubectl apply -f certificate.yaml
+
+
+helm upgrade prometheus prometheus/kube-prometheus-stack -f values.yaml
